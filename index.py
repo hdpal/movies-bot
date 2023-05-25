f4c93640c994777cb712dbda784d0835803887dc
hdpal <134629346+hdpal@users.noreply.github.com>
2023-05-25 22:11:18 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://movies-bot-seven-umber.vercel.app/"
+URL = "https://movies-bot-seven-umber.vercel.app"
 bot = Bot(TOKEN)
 
 
